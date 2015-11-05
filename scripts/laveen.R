64560faceba5aff3047246e975f2a87fbdcc5e5f
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-11-04 17:09:35 -0800

    playing with laveen to code most basic model prior to bayes

---
+++ scripts/laveen.R
@@ -0,0 +1,13 @@
+# shadow_net
+# network modeling with RNA-seq data
+# basic graph model first
+
+library(lavaan)
+
+ls()
+shade <- SAS.expression.vst.s.kazu.largeCV.modules
+head(shade)
+tail(shade)
+
+# drew out model
+# translate to pseudo code on white board.
